ea840b9452059c40f0e3492d75e99a182e6dc71e
pedrocel <pedrinhonovais10@gmail.com>
2025-03-21 02:44:40 -0300

    use seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\OrganizationModel;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\PerfilModel;
+use App\Models\UserOrganizationModel;
 use App\Models\UserPerfilModel;
 
 class UserSeeder extends Seeder
@@ -17,11 +19,23 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $org = OrganizationModel::create([
+            'name' => 'Select Seguros Alto',
+            'description' => 'Organização principal',
+            'status' => 1
+        ]);
+
         // Criação do usuário
         $user = User::create([
             'name' => 'Pedro vinicius de souza novais',
             'email' => 'pedronovais@kernelcode.com.br',
             'password' => Hash::make('123456789'),
+        ]);
+
+        UserOrganizationModel::create([
+            'user_id' => $user->id, 
+            'organization_id' => $org->id, 
+            'status' => 1
         ]);
 
         UserPerfilModel::create([
